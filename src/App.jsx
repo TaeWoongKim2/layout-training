93c7a6e30efd7edecc74f88a870ff023496d042e
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-21 18:00:36 +0900

    Navigator 컴포넌트 파일만 분리.
    - 관심사 분리는 다음 작업에서 진행할 예정!

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,12 @@ import { Link } from 'react-router-dom';
 
 import './App.css';
 
-import {
-  Menu,
-  Close,
-  LibraryOutline,
-  LogoGithub,
-  LogoGoogle,
-} from 'react-ionicons';
+import { Menu } from 'react-ionicons';
+
+import NavMenu from './components/Navigator/Nav';
 
 function App() {
   const [menuActive, setMenuActive] = useState(false);
-  const [menuItemActive, setMenuItemActive] = useState({ item: '' });
 
   return (
     <>
@@ -29,71 +24,10 @@ function App() {
           style={{ display: 'flex' }}
         />
 
-        <nav
-          id="nav-menu"
-          className={`nav ${menuActive ? 'show' : ''}`}
-        >
-          <div className="nav__content bd-grid">
-            <div className="nav__toggle">
-              <Close
-                cssClasses="nav__close"
-                color="var(--white-color)"
-                onClick={() => setMenuActive(false)}
-              />
-            </div>
-
-            <div className="nav__perfil">
-              <div className="nav__img">
-                <img src="../public/images/perfil.png" alt="" />
-              </div>
-
-              <div>
-                <Link to="#name" className="nav__name">EHOTO</Link>
-                <span className="nav__profession">Front Developer</span>
-              </div>
-            </div>
-
-            <div className="nav__menu">
-              <ul className="nav__list">
-                {['Home', 'About', 'Skills', 'Portfolio', 'Contact'].map((item) => (
-                  <li
-                    key={item}
-                    className={`nav__item ${menuItemActive === item ? 'active' : ''}`}
-                  >
-                    <Link
-                      to={`#${item}`}
-                      className="nav__link"
-                      onClick={() => setMenuItemActive({ item })}
-                    >
-                      { item }
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="nav__social">
-              <Link to="#tistory" className="nav__social-icon">
-                <LibraryOutline
-                  title="Tistory"
-                  color="var(--white-color)"
-                />
-              </Link>
-              <Link to="#github" className="nav__social-icon">
-                <LogoGithub
-                  title="Github"
-                  color="var(--white-color)"
-                />
-              </Link>
-              <Link to="#gmail" className="nav__social-icon">
-                <LogoGoogle
-                  title="Gmail"
-                  color="var(--white-color)"
-                />
-              </Link>
-            </div>
-          </div>
-        </nav>
+        <NavMenu
+          active={menuActive}
+          onClick={setMenuActive}
+        />
       </header>
     </>
   );
