013e246716de0f0969f0f1d33383f7ddfc61c261
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-27 10:33:40 +0900

    하단 네비게이션 HTML 작성.
    - Bottom navigator 연습을 위해 HTML만 작성진행했습니다.

--- src/routers/Portfolio.jsx
+++ src/routers/Portfolio.jsx
@@ -2,6 +2,44 @@ import React from 'react';
 
 export default function Portfolio() {
   return (
-    <h2>Portfolio!</h2>
+    <>
+      <h2>Portfolio!</h2>
+
+      <div className="workspace">
+        <ul className="bottom__nav">
+          <span className="bottom__nav__indicator" />
+          <li>
+            <a href="#home">
+              <i className="bx bx-home" />
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              <i className="bx bx-about" />
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills">
+              <i className="bx bx-skill" />
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio">
+              <i className="bx bx-portfolio" />
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#contact">
+              <i className="bx bx-contact" />
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
