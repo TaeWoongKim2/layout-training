4a7906ff18b4cbb53326f65d2c01cc2bef2238e9
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-27 21:27:55 +0900

    Bottom Navigator 코드 리팩토링
    - 구조 분해 할당 문법 적용.
    - 조금 더 깔끔하고, 일기 쉽게 리팩토링.

--- src/routers/Portfolio.jsx
+++ src/routers/Portfolio.jsx
@@ -10,59 +10,72 @@ import {
   CafeOutline,
 } from 'react-ionicons';
 
+const defaultBottomNav = {
+  about: {
+    title: 'About',
+    link: '#about',
+    icon: <BulbOutline color="inherit" />,
+    active: false,
+  },
+  skills: {
+    title: 'Skills',
+    link: '#skills',
+    icon: <CodeSlashOutline color="inherit" />,
+    active: false,
+  },
+  home: {
+    title: 'Home',
+    link: '#home',
+    icon: <HomeOutline color="inherit" />,
+    active: false,
+  },
+  portfolio: {
+    title: 'Portfolio',
+    link: '#portfolio',
+    icon: <QrCodeOutline color="inherit" />,
+    active: false,
+  },
+  contact: {
+    title: 'Contact',
+    link: '#contact',
+    icon: <CafeOutline color="inherit" />,
+    active: false,
+  },
+};
+
+const changeNavActive = (item) => ({
+  ...defaultBottomNav,
+  [item]: {
+    ...defaultBottomNav[item],
+    active: true,
+  },
+});
+
 export default function Portfolio() {
-  const [bottomNav, setBottomNav] = useState({
-    about: {
-      title: 'About',
-      link: '#about',
-      icon: <BulbOutline color="inherit" />,
-      active: false,
-    },
-    skills: {
-      title: 'Skills',
-      link: '#skills',
-      icon: <CodeSlashOutline color="inherit" />,
-      active: false,
-    },
-    home: {
-      title: 'Home',
-      link: '#home',
-      icon: <HomeOutline color="inherit" />,
-      active: true,
-    },
-    portfolio: {
-      title: 'Portfolio',
-      link: '#portfolio',
-      icon: <QrCodeOutline color="inherit" />,
-      active: false,
-    },
-    contact: {
-      title: 'Contact',
-      link: '#contact',
-      icon: <CafeOutline color="inherit" />,
-      active: false,
-    },
-  });
+  const [bottomNav, setBottomNav] = useState(
+    changeNavActive('home'),
+  );
 
   function handleClick(item) {
-    setBottomNav({ item });
+    const newBottonNav = changeNavActive(item);
+    setBottomNav(newBottonNav);
   }
 
   return (
     <>
       <ul className="bottom__nav">
         <span className="bottom__nav__indicator" />
-        {Object.keys(bottomNav).map((navItem) => {
+        {Object.keys(bottomNav).map((nav) => {
           const {
-            key, link, title, icon, active,
-          } = bottomNav[navItem];
+            link, title, icon, active,
+          } = bottomNav[nav];
 
           return (
-            <li key={key}>
+            <li key={nav}>
               <Link
                 to={`${link}`}
                 className={`${active ? 'bottom__nav__item__active' : ''}`}
-                onClick={() => handleClick(key)}
+                onClick={() => handleClick(nav)}
               >
                 {icon}
                 <span className="bottom__nav__title">{title}</span>
