215e1f7ead8fed8e493ff658dd9a64003366735b
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-27 18:19:32 +0900

    바텀 네비게이터 리팩토링 작업.
    - `const`를 통한 데이터화 후 렌더링하도록 변경 진행.
      - `const data = {}`
      - `Object.keys().map(() => {})`를 통해 렌더링.

--- src/routers/Portfolio.jsx
+++ src/routers/Portfolio.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Link } from 'react-router-dom';
 
 import {
   HomeOutline,
@@ -9,17 +11,66 @@ import {
 } from 'react-ionicons';
 
 export default function Portfolio() {
+  const [bottomNav, setBottomNav] = useState({
+    about: {
+      title: 'About',
+      link: '#about',
+      icon: <BulbOutline color="inherit" />,
+      active: false,
+    },
+    skills: {
+      title: 'Skills',
+      link: '#skills',
+      icon: <CodeSlashOutline color="inherit" />,
+      active: false,
+    },
+    home: {
+      title: 'Home',
+      link: '#home',
+      icon: <HomeOutline color="inherit" />,
+      active: true,
+    },
+    portfolio: {
+      title: 'Portfolio',
+      link: '#portfolio',
+      icon: <QrCodeOutline color="inherit" />,
+      active: false,
+    },
+    contact: {
+      title: 'Contact',
+      link: '#contact',
+      icon: <CafeOutline color="inherit" />,
+      active: false,
+    },
+  });
+
+  function handleClick(item) {
+    setBottomNav({ item });
+  }
+
   return (
     <>
       <ul className="bottom__nav">
         <span className="bottom__nav__indicator" />
-        <li>
-          <a href="#home">
-            <HomeOutline color="inherit" />
-            <span className="bottom__nav__title">Home</span>
-          </a>
-        </li>
-        <li>
+        {Object.keys(bottomNav).map((navItem) => {
+          const {
+            key, link, title, icon, active,
+          } = navItem;
+
+          return (
+            <li key={key}>
+              <Link
+                to={`${link}`}
+                className={`${active ? 'bottom__nav__item__active' : ''}`}
+                onClick={() => handleClick(key)}
+              >
+                {icon}
+                <span className="bottom__nav__title">{title}</span>
+              </Link>
+            </li>
+          );
+        })}
+        {/* <li>
           <a href="#about">
             <BulbOutline color="inherit" />
             <span className="bottom__nav__title">About</span>
@@ -33,6 +84,12 @@ export default function Portfolio() {
           </a>
         </li>
         <li>
+          <a href="#home" className="bottom__nav__item__active">
+            <HomeOutline color="inherit" />
+            <span className="bottom__nav__title">Home</span>
+          </a>
+        </li>
+        <li>
           <a href="#portfolio">
             <QrCodeOutline color="inherit" />
             <span className="bottom__nav__title">Portfolio</span>
@@ -43,7 +100,7 @@ export default function Portfolio() {
             <CafeOutline color="inherit" />
             <span className="bottom__nav__title">Contact</span>
           </a>
-        </li>
+        </li> */}
       </ul>
     </>
   );
