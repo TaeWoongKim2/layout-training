538e7cf5c32108406f657e2f24c542365e7b1eef
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-27 15:02:32 +0900

    Bottom Nav inline 및 아이콘 변경.
    - `inline-grid`

--- src/routers/Portfolio.jsx
+++ src/routers/Portfolio.jsx
@@ -2,51 +2,49 @@ import React from 'react';
 
 import {
   HomeOutline,
-  FingerPrintOutline,
+  BulbOutline,
   CodeSlashOutline,
   QrCodeOutline,
-  PhonePortraitOutline,
+  CafeOutline,
 } from 'react-ionicons';
 
 export default function Portfolio() {
   return (
     <>
-      <div className="workspace">
-        <ul className="bottom__nav">
-          <span className="bottom__nav__indicator" />
-          <li>
-            <a href="#home">
-              <HomeOutline color="#00000" />
-              <span className="bottom__nav__title">Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="#about">
-              <FingerPrintOutline color="#00000" />
-              <span className="bottom__nav__title">About</span>
-            </a>
-          </li>
-          <li>
-            <a href="#skills">
-              <CodeSlashOutline color="#00000" />
-              <i className="bx bx-code-curly" />
-              <span className="bottom__nav__title">Skills</span>
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio">
-              <QrCodeOutline color="#00000" />
-              <span className="bottom__nav__title">Portfolio</span>
-            </a>
-          </li>
-          <li>
-            <a href="#contact">
-              <PhonePortraitOutline color="#00000" />
-              <span className="bottom__nav__title">Contact</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className="bottom__nav">
+        <span className="bottom__nav__indicator" />
+        <li>
+          <a href="#home">
+            <HomeOutline color="inherit" />
+            <span className="bottom__nav__title">Home</span>
+          </a>
+        </li>
+        <li>
+          <a href="#about">
+            <BulbOutline color="inherit" />
+            <span className="bottom__nav__title">About</span>
+          </a>
+        </li>
+        <li>
+          <a href="#skills">
+            <CodeSlashOutline color="inherit" />
+            <i className="bx bx-code-curly" />
+            <span className="bottom__nav__title">Skills</span>
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio">
+            <QrCodeOutline color="inherit" />
+            <span className="bottom__nav__title">Portfolio</span>
+          </a>
+        </li>
+        <li>
+          <a href="#contact">
+            <CafeOutline color="inherit" />
+            <span className="bottom__nav__title">Contact</span>
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
