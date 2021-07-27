0377abaa98c1acae117261501b1df11e36d85f82
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-27 21:29:19 +0900

    불필요한 주석 제거.
    - <li> 태그 반복 나열 코드 제거.

--- src/routers/Portfolio.jsx
+++ src/routers/Portfolio.jsx
@@ -83,37 +83,6 @@ export default function Portfolio() {
             </li>
           );
         })}
-        {/* <li>
-          <a href="#about">
-            <BulbOutline color="inherit" />
-            <span className="bottom__nav__title">About</span>
-          </a>
-        </li>
-        <li>
-          <a href="#skills">
-            <CodeSlashOutline color="inherit" />
-            <i className="bx bx-code-curly" />
-            <span className="bottom__nav__title">Skills</span>
-          </a>
-        </li>
-        <li>
-          <a href="#home" className="bottom__nav__item__active">
-            <HomeOutline color="inherit" />
-            <span className="bottom__nav__title">Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="#portfolio">
-            <QrCodeOutline color="inherit" />
-            <span className="bottom__nav__title">Portfolio</span>
-          </a>
-        </li>
-        <li>
-          <a href="#contact">
-            <CafeOutline color="inherit" />
-            <span className="bottom__nav__title">Contact</span>
-          </a>
-        </li> */}
       </ul>
     </>
   );
